0c8cb91aff3d3ce352c72622f165cfd712d26edf
Lorenzo <hello@lorenzo0111.me>
2024-07-20 09:24:54 +0200

    Project setup - Created empty next app - Removed useless things - ShadCN setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import { cn } from "@/lib/utils";
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata = {
+  title: "YML Editor",
+  description: "Edit your yml from a web editor",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
